6020f1ef25ebcdd64a46ea7c73cfdef2b66ad7be
Dio <diorama@detik.com>
2024-02-21 16:21:38 +0700

    test reload

--- preload.js
+++ preload.js
@@ -2,6 +2,12 @@
 // renderer process
 const { ipcRenderer  } = require ("electron");
 
+let scrollLoop = 0;
+let scrollSIHandler = null;
+let scrollHeight = 250;
+let scrollInterval = 750;
+let reloadTimeout = 2000;
+
 // All the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
@@ -16,35 +22,28 @@ window.addEventListener('DOMContentLoaded', () => {
     for (const dependency of ['chrome', 'node', 'electron']) {
       replaceText(`${dependency}-version`, process.versions[dependency])
     }
-  }
-});
-
-let scrollLoop = 0;
-let scrollSIHandler = null;
-let scrollHeight = 250;
-let scrollInterval = 750;
-
-window.addEventListener('load', () => {
-  let siteURL = location.href;
-  console.log('hitbooster window onload siteURL', siteURL);
-  if (siteURL.indexOf('file:') === -1) {
+  } else {
     let scenario = ipcRenderer.sendSync('getScenario');
-    console.log('hitbooster window onload scenario', scenario);
+    console.log('hitbooster window DOMContentLoaded scenario', scenario);
     if (scenario === '0') {
-      console.log('hitbooster window onload scenario 0 reload');
-      location.reload(true);
+      console.log('hitbooster window DOMContentLoaded scenario 0 reload');
+      setTimeout(() => {
+        location.reload(true);
+      }, reloadTimeout);
     } else if (scenario === '1') {
-      window.scrollTo({left:0, top:0});
       var scrollSIHandler = setInterval(() => {
         if ((scrollLoop * scrollHeight) < document.body.scrollHeight) {
           window.scrollTo({left:0, top:scrollLoop * scrollHeight, behavior:'smooth'});
           scrollLoop++;
-          console.log('hitbooster window onload scenario 1 scroll', scrollLoop);
+          console.log('hitbooster window DOMContentLoaded scenario 1 scroll', scrollLoop);
         } else {
           scrollLoop = 0;
           clearInterval(scrollSIHandler);
-          console.log('hitbooster window onload scenario 1 end scroll');
-          location.reload(true);
+          window.scrollTo({left:0, top:0});
+          console.log('hitbooster window DOMContentLoaded scenario 1 end scroll');
+          setTimeout(() => {
+            location.reload(true);
+          }, reloadTimeout);
         }
       }, scrollInterval);
     }
